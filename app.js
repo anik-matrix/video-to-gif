cee0d7f00bdb40fc17dc4e5d394e08fae4d68ebe
anik-matrix <anik@matrixnmedia.com>
2022-07-29 16:59:19 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const multer = require('multer');
+const {exec} = require('child_process');
+const fs = require('fs');
+const app = express();
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "public");
+      },
+      filename: (req, file, cb) => {
+        const ext = file.mimetype.split("/")[1];
+        let originalname = file.originalname;
+        originalname = (originalname.split(".")[0]).replace(/ /g, "-");
+        cb(null, `files/${originalname}-${Date.now()}.${ext}`);
+      },
+})
+const upload = multer({
+    storage:storage
+});
+app.post("/upload-convert", upload.single("video"), (req, res) => {
+    console.log(req.file);
+    const filePath = req.file.path;
+    const startTime = Date.now();
+    let fileName = req.file.filename;
+    fileName = fileName.split("/").pop().split(".")[0];
+    // convert the file into gif
+    exec(`ffmpeg -i ${filePath} -qscale 0 public/files/${fileName}.gif`, (err, stdout, stderr) => {
+        if(err) {
+            console.log(err);
+            res.send(err);
+            return;
+        }
+        console.log(req.file.filename);
+        fs.unlink('public/' + req.file.filename, (err) => {
+            if(err) console.log(err);
+        });
+        console.log(Date.now() - startTime);
+    });
+    res.send("Works");
+});
+
+app.get("/", (req, res) => {
+    res.send("API is running...");
+});
+
+app.listen(8585, () => {
+    console.log("Listening on 8585");
+})
